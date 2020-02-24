6cd35fb0b14335d5a7387cbfd96190beb0277070
ChristianMRodas <chrismrodas@gmail.com>
2020-02-23 22:48:04 -0600

    Sprint 1 Completion

--- app/src/main/java/com/example/pwccomparisontoolapp/MainActivity.kt
+++ app/src/main/java/com/example/pwccomparisontoolapp/MainActivity.kt
@@ -34,29 +34,6 @@ class MainActivity : AppCompatActivity() {
             spinner.adapter = adapter
         }
 
-/*
-        Spinner mySpinner = (Spinner) findViewById(R.id.Makes_Spinner)
-
-        //An array adapter is bascially how the string arrays are placed into the Spinner
-        ArrayAdapter<String> = myAdapater = new ArrayAdapter<String>(MainActivity.this,
-            android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Makes))
-        myAdapter.setDropdownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        Makes_SpinnterAdapater.setAdapter(myAdapter);
-
-
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-        */
 
     }
 }
